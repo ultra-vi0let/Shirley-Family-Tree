7ce7220a312fed152039979273e516eda72db5d3
ultra-vi0let <threehundredthirtythird@gmail.com>
2022-07-18 17:15:06 -0400

    typo

--- ehrstinetree.js
+++ ehrstinetree.js
@@ -68,7 +68,7 @@ family.load( [
               { id: 19, mid: 17, fid: 16, gender: "female", name: "Celeste Joy Ellison"},
             { id: 20, pids: [17], gender: "male", name: "David Thomas Wells"},
               { id: 21, mid: 17, fid: 20, gender: "male", name: "Rush Lonnie Wells"},
-            { id: 22, pids: [23], gender: "female", name: "Carrie Mariue Vesper"},
+            { id: 22, pids: [23], gender: "female", name: "Carrie Marie Vesper"},
             { id: 23, pids: [22], mid: 14, fid: 15, gender: "male", name: "Peter Josiah Rush"},
               { id: 24, mid: 22, fid: 23, gender: "female", name: "Elizabeth Marie Rush"},
               { id: 25, mid: 22, fid: 23, gender: "female", name: "Josephine Patricia Rush"},
