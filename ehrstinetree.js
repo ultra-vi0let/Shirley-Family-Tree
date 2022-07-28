fbb78c10ebdb4044ed784fc0540d73a25d16e037
ultra-vi0let <threehundredthirtythird@gmail.com>
2022-07-28 01:57:00 -0400

    another attempt to fix ios display

--- ehrstinetree.js
+++ ehrstinetree.js
@@ -1,6 +1,6 @@
-document.addEventListener('gesturestart', function (e) {
-    e.preventDefault();
-});
+// document.addEventListener('gesturestart', function (e) {
+//     e.preventDefault();
+// });
 
 FamilyTree.elements.myTextArea = function (data, editElement, minWidth, readOnly) {
     var id = FamilyTree.elements.generateId();
